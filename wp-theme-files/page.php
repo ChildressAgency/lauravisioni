0ccb0b6c9a7613f6ea13393100176ed4676123cf
Mikhail <mmccarty@childressagency.com>
2019-07-27 13:18:34 -0400

    fix about page photo to use custom field.

--- wp-theme-files/page.php
+++ wp-theme-files/page.php
@@ -1,22 +1,22 @@
 <?php get_header(); ?>
     <section class="row">
-        <?php while (have_posts()) : the_post(); ?>
-            <?php if (get_field("about-image")) : ?>
-                <div class="col-5 page-image">
-                    <img src="<?php print_r(get_field("about-image")) ?>" alt=""/>
-                </div>
-            <?php endif ?>
-            <article class="col-7" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <h2 class="page-title"><?php the_title() ?></h2>
-                <div class="page-content">
-                    <?php the_content(); ?>
-                </div>
-            </article>
+      <?php while (have_posts()) : the_post(); ?>
+        <?php if (get_field("about_image", "option")) : ?>
+              <div class="col-5">
+                  <img class="about-image" src="<?php echo get_field("about_image", "option") ?>" alt=""/>
+              </div>
+        <?php endif ?>
+          <article class="col-7" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+              <h2 class="page-title"><?php the_title() ?></h2>
+              <div class="page-content">
+                <?php the_content(); ?>
+              </div>
+          </article>
 
-        <?php
+      <?php
 
-        endwhile; // End of the loop.
-        ?>
+      endwhile; // End of the loop.
+      ?>
     </section>
 
 <?php
