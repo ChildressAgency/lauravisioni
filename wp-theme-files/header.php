767f2345c14e78f18dc5b0b3086584a89c051448
Mikhail <mmccarty@childressagency.com>
2019-07-27 13:24:14 -0400

    some work on headers.

--- wp-theme-files/header.php
+++ wp-theme-files/header.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <head>
-  <title><?php echo get_bloginfo('name');
-    if (get_bloginfo('description')): echo ' | ' . get_bloginfo('description'); endif; ?></title>
+    <title><?php echo get_bloginfo('name');
+      if (get_bloginfo('description')): echo ' | ' . get_bloginfo('description'); endif; ?></title>
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Barlow:400,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Barlow:400,500&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
 
@@ -15,8 +15,24 @@
 <?php wp_body_open(); ?>
 
 <div class="container">
-  <header id="header" class="row <?php echo is_front_page() ? "large" : "small" ?>">
-    <div class="logo">
-      <?php if (has_custom_logo()) the_custom_logo() ?>
-    </div>
-  </header>
+  <?php if (is_front_page()): ?>
+      <header id="header" class="large">
+          <div class="logo">
+            <?php if (has_custom_logo()) the_custom_logo() ?>
+          </div>
+          <div class="row">
+              <div class="col">
+                  menu goes here
+              </div>
+          </div>
+      </header>
+  <?php else: ?>
+      <header id="header" class="small row">
+          <div class="logo col">
+            <?php if (has_custom_logo()) the_custom_logo() ?>
+          </div>
+          <div class="col">
+              menu goes here
+          </div>
+      </header>
+  <?php endif ?>
