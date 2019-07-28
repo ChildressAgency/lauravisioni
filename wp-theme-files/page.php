a490f92d7f9856b09c87ca8140a16e57c894fad6
Mikhail <mmccarty@childressagency.com>
2019-07-28 19:45:31 -0400

    seems the plugin can't be invoked by shortcode from outside a post body.

--- wp-theme-files/page.php
+++ wp-theme-files/page.php
@@ -8,5 +8,24 @@
       </article>
     <?php endwhile; ?>
   </section>
+
+<?php while (have_rows('gallery_block')): the_row(); ?>
+  <?php if (get_sub_field("gallery")): ?>
+    <section class="row home-gallery">
+      <?php
+      $code = '[phmm-project id="' . get_sub_field("gallery") . '"]';
+      echo $code;
+      echo codeneric\phmm\base\frontend\Main::gallery_shortcode($code);
+//      echo do_shortcode($code);
+      ?>
+    </section>
+  <?php endif; ?>
+  <section class="row quote">
+    <blockquote class="col-6">
+      <p><?php the_sub_field("quote_body"); ?></p>
+      <cite>&mdash;<?php the_sub_field("quote_author"); ?></cite>
+    </blockquote>
+  </section>
+<?php endwhile; ?>
 <?php
 get_footer();
