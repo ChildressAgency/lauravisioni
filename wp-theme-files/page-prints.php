cd0aa69c828c7d78b65ff059dcc6b081f637a2ac
Mikhail <mmccarty@childressagency.com>
2019-08-03 21:41:42 -0400

    finish up prints pages

--- wp-theme-files/page-prints.php
+++ wp-theme-files/page-prints.php
@@ -15,11 +15,11 @@
         $orientation = cai_get_print_orientation(get_field("image"));
         ?>
 
-        <div class="col-md-4 col-sm-12">
+        <a class="col-md-4 col-sm-12" href="<?php the_permalink(); ?>">
           <div class="print <?php echo $orientation ?>">
             <img src="<?php echo get_field("image")["sizes"]["medium"] ?>" alt="<?php echo get_the_title() ?>"/>
           </div>
-        </div>
+        </a>
 
       <?php
       endif;
--- wp-theme-files/single-print.php
+++ wp-theme-files/single-print.php
@@ -1,18 +1,27 @@
 <?php get_header(); ?>
 <?php while (have_posts()) : the_post(); ?>
-  <div class="row" id="print-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php if (get_field("image")): ?>
-      <section class="col-10 image">
-        <?php $orientation = cai_get_print_orientation(get_field("image")); ?>
+  <section class="row justify-content-start" id="print-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php if (get_field("image")):
+      $orientation = cai_get_print_orientation(get_field("image"));
+      ?>
+      <div class="col-1 prev-link">
+        <?php previous_post_link("%link", " "); ?>
+      </div>
+      <div class="col-10 image text-center">
         <img src="<?php echo get_field("image")["sizes"]["large"] ?>" alt="<?php the_title() ?>"
              class="img-fluid large-image"/>
-      </section>
-      <section class="col-md-5 col-sm-12">
+      </div>
+      <div class="col-1 next-link">
+        <?php next_post_link("%link", " "); ?>
+      </div>
+
+      <section class="col-md-5 offset-md-1 col-sm-12">
         <div class="print <?php echo $orientation ?>">
           <img src="<?php echo get_field("image")["sizes"]["medium_large"] ?>"
                alt="<?php the_title() ?>"/>
         </div>
       </section>
+
       <section class="col-md-5 col-sm-12 description">
         <h2><?php the_title() ?></h2>
         <h3><?php echo get_field("subtitle") ?></h3>
@@ -29,15 +38,15 @@
         <?php endif; ?>
       </section>
 
-      <div class="col-md-5 col-sm-12">
-        <a href="/" class="link-back">&lt; Back to All Prints</a>
-      </div>
+      <section class="col-md-5 offset-md-1 col-sm-12">
+        <a href="<?php echo get_permalink(get_page_by_path("prints")) ?>" class="link-back">&lt; Back to All Prints</a>
+      </section>
 
-      <div class="col-md-4 offset-md-1 col-sm-12">
-        <a href="/" class="button">Contact for Pricing</a>
-      </div>
+      <section class="col-md-4 offset-md-1 col-sm-12">
+        <a href="<?php echo get_permalink(get_page_by_path("contact")) ?>" class="button">Contact for Pricing</a>
+      </section>
     <?php endif; ?>
-  </div>
+  </section>
 <?php endwhile; ?>
 
 <?php get_footer();
