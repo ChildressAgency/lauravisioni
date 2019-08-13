b7d782152649dd751021849197ff5199b84ae18f
Mikhail <mmccarty@childressagency.com>
2019-08-13 19:52:37 -0400

    I had forgotten to make this page responsive.

--- wp-theme-files/template-parts/projects/auth.php
+++ wp-theme-files/template-parts/projects/auth.php
@@ -1,11 +1,11 @@
 <section class="row project">
 
   <?php if (has_post_thumbnail()): ?>
-    <img class="img-fluid col-7" alt="<?php the_title() ?>"
+    <img class="img-fluid col-md-7 col-12 mb-3" alt="<?php the_title() ?>"
          src="<?php echo get_the_post_thumbnail_url($post, "large") ?>"/>
   <?php endif; ?>
 
-  <div class="col-5 text-center project-auth">
+  <div class="col-md-5 col-12 text-center project-auth">
     <h2><?php the_title() ?></h2>
     <?php the_content(); ?>
   </div>
