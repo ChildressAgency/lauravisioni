94a9f37263cdc14377fc5983f663be0fce8468e4
Mikhail <mmccarty@childressagency.com>
2019-08-23 02:02:10 -0400

    udpate front page galleries to emit shortcodes for Modula plugin.

--- wp-theme-files/front-page.php
+++ wp-theme-files/front-page.php
@@ -10,44 +10,9 @@
   </section>
 
 <?php while (have_rows('gallery_block')): the_row(); ?>
-  <?php if (get_sub_field("gallery")):
-    $images = get_sub_field("gallery");
-    $layout = get_sub_field("layout");
-    $side = $images[0];
-    $top = $images[1];
-    $bot = $images[2];
-    ?>
-    <section class="row home-gallery">
-
-      <?php if ($side && ($layout === "left")): ?>
-        <div class="col-5 big">
-          <a href="<?php echo get_attachment_link($side["id"]) ?>"
-             style="background-image: url('<?php echo $side["sizes"]["large"] ?>')"></a>
-        </div>
-      <?php endif; ?>
-
-      <div class="col col-7 flex-column align-content-stretch">
-        <?php if ($top): ?>
-          <a href="<?php echo get_attachment_link($top["id"]) ?>">
-            <img src="<?php echo $top["sizes"]["large"] ?>" class="img-fluid"
-                 alt="<?php echo $top["title"] ?>"/>
-          </a>
-        <?php endif; ?>
-        <?php if ($bot): ?>
-          <a href="<?php echo get_attachment_link($bot["id"]) ?>">
-            <img src="<?php echo $bot["sizes"]["large"] ?>" class="img-fluid"
-                 alt="<?php echo $bot["title"] ?>"/>
-          </a>
-        <?php endif; ?>
-      </div>
-
-      <?php if ($side && ($layout !== "left")): ?>
-        <div class="col-5 big">
-          <a href="<?php echo get_attachment_link($side["id"]) ?>"
-             style="background-image: url('<?php echo $side["sizes"]["large"] ?>')"></a>
-        </div>
-      <?php endif; ?>
-
+  <?php if (get_sub_field("gallery")): ?>
+    <section>
+      <?php echo do_shortcode('[modula id="' . get_sub_field("gallery") . '"]') ?>
     </section>
   <?php endif; ?>
   <section class="row quote">
@@ -69,7 +34,8 @@
     <div class="col-md-8 col-sm-12">
       <div class="row quote">
         <section class="col-12 col-md-10 offset-md-2">
-          <img class="img-fluid contact-logo" src="<?php echo get_template_directory_uri() ?>/img/logo-med.png" alt="Logo"/>
+          <img class="img-fluid contact-logo" src="<?php echo get_template_directory_uri() ?>/img/logo-med.png"
+               alt="Logo"/>
         </section>
         <section class="col-12 col-md-10 offset-md-2 text-center text-md-left">
           <a class="button" href="<?php echo get_permalink(get_page_by_path("contact")) ?>">Contact</a>
