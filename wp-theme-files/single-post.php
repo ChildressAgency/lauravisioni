d89d8d407514dbccb4451dba7b880933d1dbbb36
childressdev <jcampbell@childressagency.com>
2020-06-23 09:39:29 -0400

    updated featured image

--- wp-theme-files/single-post.php
+++ wp-theme-files/single-post.php
@@ -7,9 +7,12 @@
       $image = has_post_thumbnail() ?
         get_the_post_thumbnail_url($post, "medium_large")
         : get_template_directory_uri() . "/img/post_default.png";
+
+      $image_alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
       ?>
 
-      <div class="featured-image col-md-10 col-sm-12" style="background-image: url('<?php echo $image ?>')"></div>
+      <!--<div class="featured-image col-md-10 col-sm-12" style="background-image: url('<?php //echo $image ?>')"></div>-->
+      <img src="<?php echo esc_url($image); ?>" class="img-fluid d-block mx-auto" alt="<?php echo esc_attr($image_alt); ?>" />
 
       <h3 class="post-date col-md-10 col-sm-12"><?php the_date() ?></h3>
 
